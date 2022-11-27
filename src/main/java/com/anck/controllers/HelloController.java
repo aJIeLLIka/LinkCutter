247de574e9cedd9b7e56aede1e8372bcc9852c9a
Antonchik <blatnoi911@mail.ru>
2022-11-27 03:31:23 +0300

    connection to Tomcat

--- src/main/java/com/anck/controllers/HelloController.java
+++ src/main/java/com/anck/controllers/HelloController.java
@@ -1,11 +1,13 @@
 package com.anck.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
+    @GetMapping("/hello")
     public String helloMethod(){
-        return "Hello world";
+        return "hello_world";
     }
 }
